db0b11c0efbc7cb45e1a78ccae9472ce3d994120
vferrucci <54486291+vferrucci@users.noreply.github.com>
2019-08-25 18:28:08 -0400

    Update RollDice.java

--- RollDice.java
+++ RollDice.java
@@ -1,6 +1,5 @@
  /**
-  *@author: Veranika Tolkach
-  *Assignment 3
+  *@author: Veronika Ferrucci
   * This program creates a game that rolls two dices
   * 10 times. If at least once out of 10 times
  * the sum of random numbers is 10, the user wins.
